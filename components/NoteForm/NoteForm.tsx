a2f5c757157ac1cc64fb970f0e07c3496e833fc6
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-05-16 13:05:44 -0400

    fix remaining api imports

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createNote, type CreateNoteData } from "@/lib/api";
+import { createNote, type CreateNoteData } from "@/lib/api/clientApi";
 import { useNoteStore } from "@/lib/store/noteStore";
 import type { NoteTag } from "@/types/note";
 import css from "./NoteForm.module.css";
